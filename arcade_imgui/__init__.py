6db5954af9ea5cbe853d97865fc91a831e78089e
Einar Forselv <eforselv@gmail.com>
2020-09-26 16:14:38 +0200

    Copy in the PygletMixin

--- arcade_imgui/__init__.py
+++ arcade_imgui/__init__.py
@@ -2,11 +2,10 @@ import ctypes
 
 import imgui
 from pyglet import gl
+from pyglet.window import key, mouse
 from arcade.gl import BufferDescription, Context
 
 from imgui.integrations import compute_fb_scale
-from imgui.integrations.opengl import ProgrammablePipelineRenderer
-from imgui.integrations.pyglet import PygletMixin
 from imgui.integrations.base import BaseOpenGLRenderer
 
 
@@ -87,7 +86,6 @@ class ArcadeGLRenderer(BaseOpenGLRenderer):
             self._ibo.write(idx_arr)
 
             idx_pos = 0
-            # print('---')
             for command in commands.commands:
                 # Use low level pyglet call here instead because we only have the texture name
                 gl.glBindTexture(gl.GL_TEXTURE_2D, command.texture_id)
@@ -100,7 +98,6 @@ class ArcadeGLRenderer(BaseOpenGLRenderer):
                 gl.glScissor(int(x), int(fb_height - w), int(z - x), int(w - y))
 
                 self._vao.render(self._program, mode=self._ctx.TRIANGLES, vertices=command.elem_count, first=idx_pos)
-                # print(idx_pos, command.elem_count)
                 idx_pos += command.elem_count
 
         # Just reset scissor back to default/viewport
@@ -148,8 +145,135 @@ class ArcadeGLRenderer(BaseOpenGLRenderer):
         self._invalidate_device_objects()
 
 
+class PygletMixin:
+    REVERSE_KEY_MAP = {
+        key.TAB: imgui.KEY_TAB,
+        key.LEFT: imgui.KEY_LEFT_ARROW,
+        key.RIGHT: imgui.KEY_RIGHT_ARROW,
+        key.UP: imgui.KEY_UP_ARROW,
+        key.DOWN: imgui.KEY_DOWN_ARROW,
+        key.PAGEUP: imgui.KEY_PAGE_UP,
+        key.PAGEDOWN: imgui.KEY_PAGE_DOWN,
+        key.HOME: imgui.KEY_HOME,
+        key.END: imgui.KEY_END,
+        key.DELETE: imgui.KEY_DELETE,
+        key.SPACE: imgui.KEY_SPACE,
+        key.BACKSPACE: imgui.KEY_BACKSPACE,
+        key.RETURN: imgui.KEY_ENTER,
+        key.ESCAPE: imgui.KEY_ESCAPE,
+        key.A: imgui.KEY_A,
+        key.C: imgui.KEY_C,
+        key.V: imgui.KEY_V,
+        key.X: imgui.KEY_X,
+        key.Y: imgui.KEY_Y,
+        key.Z: imgui.KEY_Z,
+    }
 
-# class ArcadeRenderer(PygletMixin, ProgrammablePipelineRenderer):
+    def _set_pixel_ratio(self, window):
+        window_size = window.get_size()
+        self.io.display_size = window_size
+        # It is conceivable that the pyglet version will not be solely
+        # determinant of whether we use the fixed or programmable, so do some
+        # minor introspection here to check.
+        if hasattr(window, 'get_viewport_size'):
+            viewport_size = window.get_viewport_size()
+            self.io.display_fb_scale = compute_fb_scale(window_size, viewport_size)
+        elif hasattr(window, 'get_pixel_ratio'):
+            self.io.display_fb_scale = (window.get_pixel_ratio(),
+                                        window.get_pixel_ratio())
+        else:
+            # Default to 1.0 in this unlikely circumstance
+            self.io.display_fb_scale = (1.0, 1.0)
+
+    def _attach_callbacks(self, window):
+        window.push_handlers(
+            self.on_mouse_motion,
+            self.on_key_press,
+            self.on_key_release,
+            self.on_text,
+            self.on_mouse_drag,
+            self.on_mouse_press,
+            self.on_mouse_release,
+            self.on_mouse_scroll,
+            self.on_resize,
+        )
+
+    def _map_keys(self):
+        key_map = self.io.key_map
+
+        # note: we cannot use default mechanism of mapping keys
+        #       because pyglet uses weird key translation scheme
+        for value in self.REVERSE_KEY_MAP.values():
+            key_map[value] = value
+
+    def _on_mods_change(self, mods):
+        self.io.key_ctrl = mods & key.MOD_CTRL
+        self.io.key_super = mods & key.MOD_COMMAND
+        self.io.key_alt = mods & key.MOD_ALT
+        self.io.key_shift = mods & key.MOD_SHIFT
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.io.mouse_pos = x, self.io.display_size.y - y
+
+    def on_key_press(self, key_pressed, mods):
+        if key_pressed in self.REVERSE_KEY_MAP:
+            self.io.keys_down[self.REVERSE_KEY_MAP[key_pressed]] = True
+        self._on_mods_change(mods)
+
+    def on_key_release(self, key_released, mods):
+        if key_released in self.REVERSE_KEY_MAP:
+            self.io.keys_down[self.REVERSE_KEY_MAP[key_released]] = False
+        self._on_mods_change(mods)
+
+    def on_text(self, text):
+        io = imgui.get_io()
+
+        for char in text:
+            io.add_input_character(ord(char))
+
+    def on_mouse_drag(self, x, y, dx, dy, button, modifiers):
+        self.io.mouse_pos = x, self.io.display_size.y - y
+
+        if button == mouse.LEFT:
+            self.io.mouse_down[0] = 1
+
+        if button == mouse.MIDDLE:
+            self.io.mouse_down[1] = 1
+
+        if button == mouse.RIGHT:
+            self.io.mouse_down[2] = 1
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        self.io.mouse_pos = x, self.io.display_size.y - y
+
+        if button == mouse.LEFT:
+            self.io.mouse_down[0] = 1
+
+        if button == mouse.MIDDLE:
+            self.io.mouse_down[1] = 1
+
+        if button == mouse.RIGHT:
+            self.io.mouse_down[2] = 1
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        self.io.mouse_pos = x, self.io.display_size.y - y
+
+        if button == mouse.LEFT:
+            self.io.mouse_down[0] = 0
+
+        if button == mouse.MIDDLE:
+            self.io.mouse_down[1] = 0
+
+        if button == mouse.RIGHT:
+            self.io.mouse_down[2] = 0
+
+    def on_mouse_scroll(self, x, y, mods, scroll):
+        self.io.mouse_wheel = scroll
+
+    def on_resize(self, width, height):
+        self.io.display_size = width, height
+
+
 class ArcadeRenderer(PygletMixin, ArcadeGLRenderer):
     def __init__(self, window, attach_callbacks=True):
         # super().__init__()
