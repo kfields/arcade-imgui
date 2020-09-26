9148aff1801f7eddce3151bff362320fdf5f6a19
Einar Forselv <eforselv@gmail.com>
2020-09-26 18:11:01 +0200

    Image demo should use ctx.load_texture

--- examples/image.py
+++ examples/image.py
@@ -1,27 +1,14 @@
 import os
+from pathlib import Path
+
 import arcade
 import imgui
 import imgui.core
 
-import OpenGL.GL as gl
-import numpy as np
-from PIL import Image
-
 from arcade_imgui import ArcadeRenderer
 
-def load_texture_from_file(filepath):
-    im = Image.open(filepath).convert("RGB")
-    w, h = im.size
-    imdata = np.frombuffer(im.tobytes(), np.uint8)
-    texname = gl.glGenTextures(1)
-    gl.glBindTexture(gl.GL_TEXTURE_2D, texname)
+RESOURCE_PATH = Path(__file__).parent
 
-    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
-    gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT,1)
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, w, h, 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, imdata)
-
-    return texname, w, h
 
 class MyGui:
     def __init__(self, window):
@@ -29,14 +16,13 @@ class MyGui:
         # Must create or set the context before instantiating the renderer
         imgui.create_context()
         self.renderer = ArcadeRenderer(window)
+        self.texture = window.ctx.load_texture(RESOURCE_PATH / "robocute.png", flip=False)
 
     def render(self):
         imgui.new_frame()
 
         imgui.begin("Image example")
-        image_path = './robocute.png'
-        texture_id, width, height = load_texture_from_file(image_path)
-        imgui.image(texture_id, width, height)
+        imgui.image(self.texture.glo, *self.texture.size)
         imgui.end()
 
         imgui.end_frame()
@@ -50,10 +36,6 @@ class App(arcade.Window):
     def __init__(self):
         super().__init__(800, 600, "Image Example")
         self.gui = MyGui(self)
-        #
-        file_path = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(file_path)
-
 
     def on_draw(self):
         arcade.start_render()
