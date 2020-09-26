248498974f3dd7cd734f781c799d350bfc2d89b9
Einar Forselv <eforselv@gmail.com>
2020-09-26 15:26:56 +0200

    First working renderer using arcade.gl

--- arcade_imgui/__init__.py
+++ arcade_imgui/__init__.py
@@ -1,14 +1,159 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
+import ctypes
+
+import imgui
+from pyglet import gl
+from arcade.gl import BufferDescription, Context
 
 from imgui.integrations import compute_fb_scale
 from imgui.integrations.opengl import ProgrammablePipelineRenderer
 from imgui.integrations.pyglet import PygletMixin
+from imgui.integrations.base import BaseOpenGLRenderer
 
 
-class ArcadeRenderer(PygletMixin, ProgrammablePipelineRenderer):
+class ArcadeGLRenderer(BaseOpenGLRenderer):
+    """
+    A renderer using the arcade.gl module instead of PyOpenGL.
+    This is using pyglet's OpenGL bindings instead.
+    """
+
+    VERTEX_SHADER_SRC = """
+    #version 330
+    uniform mat4 ProjMtx;
+    in vec2 Position;
+    in vec2 UV;
+    in vec4 Color;
+    out vec2 Frag_UV;
+    out vec4 Frag_Color;
+    void main() {
+        Frag_UV = UV;
+        Frag_Color = Color;
+        gl_Position = ProjMtx * vec4(Position.xy, 0, 1);
+    }
+    """
+
+    FRAGMENT_SHADER_SRC = """
+    #version 330
+    uniform sampler2D Texture;
+    in vec2 Frag_UV;
+    in vec4 Frag_Color;
+    out vec4 Out_Color;
+    void main() {
+        Out_Color = Frag_Color * texture(Texture, Frag_UV.st);
+    }
+    """
+
+    def __init__(self, window, *args, **kwargs):
+        self._window = window
+        self._ctx: Context = window.ctx
+        self._program = None
+        self._vao = None
+        self._vbo = None
+        self._ibo = None
+        self._font_texture = None
+
+        super().__init__()
+
+    def render(self, draw_data):
+        io = self.io    
+
+        display_width, display_height = io.display_size
+        fb_width = int(display_width * io.display_fb_scale[0])
+        fb_height = int(display_height * io.display_fb_scale[1])
+
+        if fb_width == 0 or fb_height == 0:
+            return
+
+        self._program["ProjMtx"] = (
+            2.0 / display_width, 0.0, 0.0, 0.0,
+            0.0, 2.0 / -display_height, 0.0, 0.0,
+            0.0, 0.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0, 1.0,
+        )
+
+        draw_data.scale_clip_rects(*io.display_fb_scale)
+
+        self._ctx.enable_only(self._ctx.BLEND)
+        self._ctx.blend_func = self._ctx.BLEND_DEFAULT
+
+        self._font_texture.use(0)
+
+        for commands in draw_data.commands_lists:
+            # Write the vertex and index buffer data without copying it
+            vtx_type = ctypes.c_byte * commands.vtx_buffer_size * imgui.VERTEX_SIZE
+            idx_type = ctypes.c_byte * commands.idx_buffer_size * imgui.INDEX_SIZE
+            vtx_arr = (vtx_type).from_address(commands.vtx_buffer_data)
+            idx_arr = (idx_type).from_address(commands.idx_buffer_data)
+            self._vbo.write(vtx_arr)
+            self._ibo.write(idx_arr)
+
+            idx_pos = 0
+            # print('---')
+            for command in commands.commands:
+                # Use low level pyglet call here instead because we only have the texture name
+                gl.glBindTexture(gl.GL_TEXTURE_2D, command.texture_id)
+
+                if imgui.INDEX_SIZE != 4:
+                    raise ValueError()
+
+                # Set scissor box
+                x, y, z, w = command.clip_rect
+                gl.glScissor(int(x), int(fb_height - w), int(z - x), int(w - y))
+
+                self._vao.render(self._program, mode=self._ctx.TRIANGLES, vertices=command.elem_count, first=idx_pos)
+                # print(idx_pos, command.elem_count)
+                idx_pos += command.elem_count
+
+        # Just reset scissor back to default/viewport
+        gl.glScissor(*self._ctx.viewport)
+
+    def refresh_font_texture(self):
+        width, height, pixels = self.io.fonts.get_tex_data_as_rgba32()
+        # Old font texture will be GCed if exist
+        self._font_texture = self._ctx.texture((width, height), components=4, data=pixels)
+        self.io.fonts.texture_id = self._font_texture.glo
+        self.io.fonts.clear_tex_data()
+
+    def _create_device_objects(self):
+        self._program = self._ctx.program(
+            vertex_shader=self.VERTEX_SHADER_SRC,
+            fragment_shader=self.FRAGMENT_SHADER_SRC,
+        )
+        self._program["Texture"] = 0
+        self._vbo = self._ctx.buffer(reserve=imgui.VERTEX_SIZE * 65536)
+        self._ibo = self._ctx.buffer(reserve=imgui.INDEX_SIZE * 65536)
+        # NOTE: imgui.INDEX_SIZE is type size for the index buffer. We might need to support 8 and 16 bit
+        # but right now we are assuming 32 bit
+        self._vao = self._ctx.geometry(
+            [
+                BufferDescription(
+                    self._vbo,
+                    "2f 2f 4f1",
+                    ("Position", "UV", "Color"),
+                    normalized=("Color",)
+                ),
+            ],
+            index_buffer = self._ibo,
+        )           
+
+    def _invalidate_device_objects(self):
+        # NOTE: OpenGL resource will automatically be released
+        self._font_texture = None
+        self._vbo = None
+        self._ibo = None
+        self._vao = None
+        self._program = None
+        self.io.fonts.texture_id = 0
+
+    def shutdown(self):
+        self._invalidate_device_objects()
+
+
+
+# class ArcadeRenderer(PygletMixin, ProgrammablePipelineRenderer):
+class ArcadeRenderer(PygletMixin, ArcadeGLRenderer):
     def __init__(self, window, attach_callbacks=True):
-        super(ArcadeRenderer, self).__init__()
+        # super().__init__()
+        super().__init__(window)
         window_size = window.get_size()
         viewport_size = window.get_viewport_size()
 
