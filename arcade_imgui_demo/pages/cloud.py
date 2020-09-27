1c96f9b13b72a64fce572dfe3bf175016d354ddb
Kurtis Fields <kurtisfields@gmail.com>
2020-09-27 01:01:36 -0400

    feat: add sparks example

--- arcade_imgui_demo/pages/cloud.py
+++ arcade_imgui_demo/pages/cloud.py
@@ -9,6 +9,7 @@ import imgui
 import imgui.core
 
 from arcade_imgui_demo.page import Page
+from arcade_imgui_demo.particle import AnimatedAlphaParticle
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -19,50 +20,6 @@ CLOUD_TEXTURES = [
     arcade.make_soft_circle_texture(50, arcade.color.LIGHT_GRAY),
     arcade.make_soft_circle_texture(50, arcade.color.LIGHT_BLUE),
 ]
-
-def clamp(a, low, high):
-    if a > high:
-        return high
-    elif a < low:
-        return low
-    else:
-        return a
-
-class AnimatedAlphaParticle(arcade.LifetimeParticle):
-    """A custom particle that animates between three different alpha levels"""
-
-    def __init__(
-            self,
-            filename_or_texture: arcade.FilenameOrTexture,
-            change_xy: Vector,
-            start_alpha: int = 0,
-            duration1: float = 1.0,
-            mid_alpha: int = 255,
-            duration2: float = 1.0,
-            end_alpha: int = 0,
-            center_xy: Point = (0.0, 0.0),
-            angle: float = 0,
-            change_angle: float = 0,
-            scale: float = 1.0,
-            mutation_callback=None,
-    ):
-        super().__init__(filename_or_texture, change_xy, duration1 + duration2, center_xy, angle, change_angle, scale,
-                         start_alpha, mutation_callback)
-        self.start_alpha = start_alpha
-        self.in_duration = duration1
-        self.mid_alpha = mid_alpha
-        self.out_duration = duration2
-        self.end_alpha = end_alpha
-
-    def update(self):
-        super().update()
-        if self.lifetime_elapsed <= self.in_duration:
-            u = self.lifetime_elapsed / self.in_duration
-            self.alpha = clamp(arcade.lerp(self.start_alpha, self.mid_alpha, u), 0, 255)
-        else:
-            u = (self.lifetime_elapsed - self.in_duration) / self.out_duration
-            self.alpha = clamp(arcade.lerp(self.mid_alpha, self.end_alpha, u), 0, 255)
-
 
 class CloudPage(Page):
     def __init__(self, window):
---
+++ arcade_imgui_demo/pages/sparks.py
@@ -0,0 +1,89 @@
+import arcade
+from arcade import Point, Vector
+from arcade.utils import _Vec2  # bring in "private" class
+import os
+import random
+import pyglet
+
+import imgui
+import imgui.core
+
+from arcade_imgui_demo.page import Page
+from arcade_imgui_demo.particle import AnimatedAlphaParticle
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Particle based fireworks"
+
+RAINBOW_COLORS = (
+    arcade.color.ELECTRIC_CRIMSON,
+    arcade.color.FLUORESCENT_ORANGE,
+    arcade.color.ELECTRIC_YELLOW,
+    arcade.color.ELECTRIC_GREEN,
+    arcade.color.ELECTRIC_CYAN,
+    arcade.color.MEDIUM_ELECTRIC_BLUE,
+    arcade.color.ELECTRIC_INDIGO,
+    arcade.color.ELECTRIC_PURPLE,
+)
+
+SPARK_TEXTURES = [arcade.make_circle_texture(8, clr) for clr in RAINBOW_COLORS]
+SPARK_PAIRS = [
+    [SPARK_TEXTURES[0], SPARK_TEXTURES[3]],
+    [SPARK_TEXTURES[1], SPARK_TEXTURES[5]],
+    [SPARK_TEXTURES[7], SPARK_TEXTURES[2]],
+]
+
+def firework_spark_mutator(particle: arcade.FadeParticle):
+    """mutation_callback shared by all fireworks sparks"""
+    # gravity
+    particle.change_y += -0.03
+    # drag
+    particle.change_x *= 0.92
+    particle.change_y *= 0.92
+
+class SparksPage(Page):
+    def __init__(self, window):
+        super().__init__(window, "sparks", "Sparks")
+
+        arcade.set_background_color(arcade.color.BLACK)
+        self.reset()
+
+    def reset(self):
+        self.create_emitter()
+
+    def create_emitter(self):
+        spark_texture = random.choice(SPARK_TEXTURES)
+        self.emitter = arcade.Emitter(
+            #center_xy=prev_emitter.get_pos(),
+            center_xy=(500, 500),
+            emit_controller=arcade.EmitBurst(random.randint(30, 40)),
+            particle_factory=lambda emitter: arcade.FadeParticle(
+                filename_or_texture=spark_texture,
+                change_xy=arcade.rand_in_circle((0.0, 0.0), 9.0),
+                lifetime=random.uniform(0.5, 1.2),
+                mutation_callback=firework_spark_mutator
+            )
+        )
+
+    def update(self, delta_time):
+        if self.emitter.center_x > SCREEN_WIDTH:
+            self.emitter.center_x = 0
+        self.emitter.update()
+
+    def on_render(self):
+        imgui.set_next_window_position(self.window.width - 288, 32, imgui.ONCE)
+        imgui.set_next_window_size(256, 256, imgui.ONCE)
+
+        imgui.begin("Sparks")
+        if imgui.button("Run"):
+            self.reset()
+        imgui.end()
+
+        self.emitter.draw()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.ESCAPE:
+            arcade.close_window()
+
+def install(app):
+    app.add_page(SparksPage(app))
