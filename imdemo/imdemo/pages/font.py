36c077da24865fb2d78d51ddcde69ca74ba6cce5
Kurtis Fields <kurtisfields@gmail.com>
2020-10-11 20:07:04 -0400

    fix: font

--- imdemo/imdemo/pages/font.py
+++ imdemo/imdemo/pages/font.py
@@ -6,14 +6,14 @@ import imgui.core
 
 from imdemo.page import Page
 
-RESOURCE_PATH = Path(__file__).parent.parent / 'assets'
+RESOURCE_PATH = Path(__file__).parent / 'resources'
 
 class FontPage(Page):
     def __init__(self, window, name, title):
         super().__init__(window, name, title)
         io = imgui.get_io()
-
-        self.new_font = io.fonts.add_font_from_file_ttf(str(RESOURCE_PATH / "DroidSans.ttf"), 20)
+        font_path = window.resource_path / 'DroidSans.ttf'
+        self.new_font = io.fonts.add_font_from_file_ttf(str(font_path), 20)
         self.window.gui.renderer.refresh_font_texture()
 
     def draw(self):
--- imdemo/imdemo/pages/fontimage.py
+++ imdemo/imdemo/pages/fontimage.py
@@ -10,7 +10,7 @@ class FontImage(Page):
         imgui.begin("Image example")
         texture_id = imgui.get_io().fonts.texture_id
         draw_list = imgui.get_window_draw_list()
-        draw_list.add_image(texture_id, (20, 35), (180, 80), col=imgui.get_color_u32_rgba(0.5,0.5,1,1))
+        draw_list.add_image(texture_id, (128, 64), (512, 256), col=imgui.get_color_u32_rgba(0.5,0.5,1,1))
         imgui.end()
 
 def install(app):
